b2099544eedf2d1a09f9ad670ebc19f899e63e48
Andrew Wei <andrewwei1999@gmail.com>
2020-05-24 21:49:52 -0700

    adding button handlers and outline of an application, still need to focus on detection

--- app/src/main/java/com/example/bathroomfalldetection/MainActivity.java
+++ app/src/main/java/com/example/bathroomfalldetection/MainActivity.java
@@ -7,8 +7,10 @@ import android.media.AudioFormat;
 import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Environment;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import java.io.File;
@@ -16,19 +18,32 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
+/**
+ * App for detecting bathroom falls and contacting people if alert isn't muted.
+ *
+ * Workflow:
+ * When app is turned on, start fall detection mechanisms.
+ * If fall detected -> start sound alert and timer for contacting emergency contact
+ *
+ * If fall detected and time runs out to cancel -> Contact emergency contact.
+ *  - If response from contact, parse response somehow or something
+ *  - If no response from contact after a few minutes, contact emergency services
+ *
+ * If fall detected and muted -> Goes back to fall detection mode
+ *
+ * Stops detecting when user quits the app.
+ *
+ */
 public class MainActivity extends AppCompatActivity {
     final Runnable detector = new Runnable() {
         public void run() {
@@ -44,6 +59,15 @@ public class MainActivity extends AppCompatActivity {
     PriorityQueue<Double> reversedSortedWindow = new PriorityQueue<>();
     List<Double> median_list = new ArrayList<>();
 
+    CountDownTimer contactTimer;
+    private boolean contactRunning;
+    final int CONTACT_COUNTDOWN = 1 * 60 * 1000; // in ms
+    final int PERIODIC_ALERT = 1 * 1000; // in ms
+
+    CountDownTimer emergencyTimer;
+    private boolean emergencyRunning;
+    final int EMERGENCY_COUNTDOWN = 4 * 60 * 1000; // in ms
+
     final int L = 17;
     final double THRESHOLD = 0.000225;
     final int SAMPLE_RATE = 44100;
@@ -57,7 +81,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Load contact information from file if available, else ask or something
+        // TODO: Also load address
+
+        // TODO: Pair with bluetooth device if available.
+
+        // Gets recording permissions
         requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, 0);
+
+        // Gets recording parameters
         int minSize = AudioRecord.getMinBufferSize(
                 SAMPLE_RATE,
                 AudioFormat.CHANNEL_IN_MONO,
@@ -193,16 +226,142 @@ public class MainActivity extends AppCompatActivity {
 //                } else {
 ////                    changeText("no_alert");
 //                }
+
+                // TODO: Fall detected
+                boolean fallDetected = false;
+
+                // Starts timer to contact emergency contact
+                // Changes text of the fall detection status
+                // Starts contact timers after
+                 if (fallDetected && !(contactRunning | emergencyRunning)) {
+                      ((TextView) findViewById(R.id.fall_detection_status)).setText(R.string.status_fall_detected);
+                      contactRunning = true;
+                      contactTimer = new CountDownTimer(CONTACT_COUNTDOWN, PERIODIC_ALERT) {
+                            @Override
+                            public void onTick(long millisUntilFinished) {
+                                // Play a sound
+                                Log.d("contactTimer", "Time left: " + millisUntilFinished);
+                                // Play a sound
+                                alertSound();
+                                // TODO: Update the contact countdown timer text.
+                            }
+
+                            @Override
+                            public void onFinish() {
+                                contactRunning = false;
+                                emergencyRunning = true;
+                                // TODO: Contact emergency contact
+                                contactSavedContact(); // TODO: Maybe provide a message
+
+                                // Start new timer for waiting for response or something when timer runs out, contact emergency services
+                                // TODO: Update the contact countdown timer text.
+                                // TODO: Update status
+                                emergencyTimer = new CountDownTimer(EMERGENCY_COUNTDOWN, PERIODIC_ALERT) {
+                                    @Override
+                                    public void onTick(long millisUntilFinished) {
+                                        Log.d("emergencyTimer", "Time left: " + millisUntilFinished);
+                                        // Play a sound
+                                        alertSound();
+                                        // TODO: Update the contact countdown timer text.
+                                    }
+
+                                    @Override
+                                    public void onFinish() {
+                                        // TODO: Text police or something with address
+                                        // TODO: Start listener for response?
+                                        // If that doesn't work, maybe have it play an audio file saying that the person fell and may be unconscious
+                                        // TODO: Update status
+
+                                    }
+                                }.start();
+                            }
+                        }.start();
+                 }
             }
         }
     }
 
     public void changeText(String text) {
-        ((TextView) findViewById(R.id.displayBox)).setText(text);
+        ((TextView) findViewById(R.id.fall_detection_status)).setText(text);
     }
 
+    /**
+     * Cancels the alert
+     */
+    public void muteHandler(View view) {
+        mute();
+    }
 
-    /*
+    /**
+     * TODO: Handles mute from bluetooth device somehow
+     */
+    public void handleBluetoothMute() {
+        // Might need something related to a dedicated listener
+        mute();
+    }
+
+    public void mute() {
+        // After fall detected, app changes text box into fall detected
+        // Pressing button will change to no fall detected
+        // Reset emergency contact timer
+
+        ((TextView)findViewById(R.id.fall_detection_status)).setText(R.string.status_no_fall_detected);
+        // disable contact timer
+        if (contactTimer != null && contactRunning) {
+            contactRunning = false;
+            contactTimer.cancel();
+        }
+
+        // disable emergency service timer
+        if (emergencyTimer != null && emergencyRunning) {
+            emergencyRunning = false;
+            emergencyTimer.cancel();
+            // TODO: Maybe send a text like "Wait, nvm" to the saved contact
+
+        }
+    }
+
+    /**
+     * TODO: Saves emergency contact and address information
+     */
+    public void saveContactHandler(View view) {
+        // Check that a valid number was entered
+    }
+
+    /**
+     * TODO: Contacts saved contact
+     */
+    public void contactSavedContact() {
+        // TODO: Has to listen for reply or something. Also send address
+    }
+
+    /**
+     * TODO: Text emergency services
+     */
+    public void textEmergencyServices() {
+        // TODO: Has to listen for bounce-back or something. Also send address
+    }
+
+    /**
+     * TODO: Call emergency services
+     */
+    public void callEmergencyServices() {
+        // TODO: After successful connection, play audio file saying that someone fell or something
+        // Still needs to bypass user settings to play audio files
+        // TODO: Need available audio file: "User fell in bathroom..."
+
+    }
+
+    /**
+     * TODO: Plays a sound that lasts 0.5 seconds during the timer
+     * Note: Should bypass the user settings to still alert while muted.
+     * Maybe also throw in some flashing lights for the sake of accessibility.
+     */
+    public void alertSound() {
+
+    }
+
+    /**
        Utility methods
      */
     private void writeToFile(String data, String filename) {
